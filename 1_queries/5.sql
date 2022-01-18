d33208f7fe341687ced92f9c3268e43fa13a8bfa
isissi <isissi_525@hotmail.com>
2022-01-18 11:14:16 -0700

    remove extra space

--- 1_queries/5.sql
+++ 1_queries/5.sql
@@ -17,11 +17,10 @@
 --  1010127 | Piano principle |          16669 | 2014-08-17 | 3.7777777777777778
 --  1012625 | Bow forest      |          13562 | 2015-07-30 | 4.2727272727272727
 
-SELECT  reservations.id, properties.title, properties.cost_per_night, reservations.start_date, 
-avg(property_reviews.rating) as average_rating
+SELECT reservations.id, properties.title, properties.cost_per_night, reservations.start_date, avg(rating) as average_rating
 FROM reservations
-JOIN properties ON properties.id = reservations.property_id
-JOIN property_reviews ON property_reviews.property_id = properties.id
+JOIN properties ON reservations.property_id = properties.id
+JOIN property_reviews ON properties.id = property_reviews.property_id
 WHERE reservations.guest_id = 1
 GROUP BY properties.id, reservations.id
 ORDER BY reservations.start_date
