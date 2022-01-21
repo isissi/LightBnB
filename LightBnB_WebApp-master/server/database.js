237aee952bd11d46ff08ccf17cab61c62dc7fddb
isissi <isissi_525@hotmail.com>
2022-01-21 11:44:18 -0700

    update getAllReservations

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -78,7 +78,23 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  // return getAllProperties(null, 2);
+  const value = [guest_id, limit];
+  const queryString = `SELECT reservations.*, properties.*, avg(rating) as average_rating
+  FROM reservations
+  JOIN properties ON reservations.property_id = properties.id
+  JOIN property_reviews ON properties.id = property_reviews.property_id
+  WHERE reservations.guest_id = $1
+  GROUP BY properties.id, reservations.id
+  ORDER BY reservations.start_date
+  LIMIT $2;`;
+  
+  return pool
+  .query(queryString, value)
+  .then(res => {
+    return res.rows;
+  })
+
 }
 exports.getAllReservations = getAllReservations;
 
